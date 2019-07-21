c5308c8cb1d50797ea6be24bcfdd9f3e740f4c10
Rasha Moumneh <r.moumneh@gmail.com>
2019-07-20 22:46:16 -0400

    complete MVP

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,7 +1,7 @@
 // STEP 3: Create Article cards.
 // -----------------------
 // Send an HTTP GET request to the following address: https://lambda-times-backend.herokuapp.com/articles
-// Stduy the response data you get back, closely.
+// Study the response data you get back, closely.
 // You will be creating a component for each 'article' in the list.
 // This won't be as easy as just iterating over an array though.
 // Create a function that will programmatically create the following DOM component:
@@ -17,3 +17,54 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+
+
+class Article {
+  constructor(article) {
+    article.forEach(article => {
+    this.article = article;
+    this.cardsContainer = document.querySelector('.cards-container')
+    this.createArticle()
+    }) 
+  }  
+
+  createArticle() {
+    
+    this.card = document.createElement('div');
+    this.card.classList.add('card');
+    this.headline = document.createElement('div')
+    this.headline.classList.add('headline')
+    
+    this.authorContainer = document.createElement('div')
+    this.authorContainer.classList.add('author')
+
+    this.imgContainer = document.createElement('div')
+    this.imgContainer.classList.add('img-container')
+    this.authorImg = document.createElement('img')
+    
+    this.authorName = document.createElement('span')
+    
+    this.headline.textContent = this.article.headline
+    this.authorImg.src = this.article.authorPhoto
+    this.authorName.textContent = this.article.authorName
+
+    this.cardsContainer.appendChild(this.card);
+    this.card.appendChild(this.headline)
+    this.card.appendChild(this.authorContainer)
+    this.authorContainer.appendChild(this.imgContainer)
+    this.imgContainer.appendChild(this.authorImg)
+    this.authorContainer.appendChild(this.authorName)
+  }       
+}
+  
+
+const lambdaArticles = axios.get('https://lambda-times-backend.herokuapp.com/articles')
+  .then(articles => {
+    let arrayofTopics = Object.keys(articles.data.articles)
+    arrayofTopics.forEach(topic => new Article(articles.data.articles[topic]))
+  })
+  .catch(err => {
+    console.log('error: ', err)
+  })
+
--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -13,26 +13,26 @@
 class Tab {
   constructor(topicData){
     this.topicData = topicData;
-    console.log(this.topicData);
     this.topicWrapper = document.querySelector('.topics');
     
     this.createTab();
   }
   createTab() {
     this.topicData.topics.forEach(topic => {
-      this.tab = document.createElement('div');
-      this.tab.classList.add('tab');
-      this.tab.textContent = topic;
-      this.topicWrapper.appendChild(this.tab); 
+    this.tab = document.createElement('div');
+    this.tab.classList.add('tab');
+    this.tab.textContent = topic;
+    this.topicWrapper.appendChild(this.tab); 
     }) 
   }
 };
 
   const lambdaTimes = axios.get('https://lambda-times-backend.herokuapp.com/topics')
-    .then((data) => {
-      let topicsArray =[];
-      topicsArray.push(data.data.topics);
-      topicsArray.forEach(topic => new Tab(data.data));
+    .then((lambdaData) => {
+      // console.log('lambdaData.data', lambdaData.data)
+      let topicsArray =[lambdaData.data.topics];
+      // console.log('lambdatopicsarray', topicsArray);
+      topicsArray.forEach(topic => new Tab(lambdaData.data));
     })
     .catch((err) => {
       console.log('error: ', err);
